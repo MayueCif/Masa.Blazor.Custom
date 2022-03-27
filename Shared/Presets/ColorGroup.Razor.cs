f29f63601c2465c8828ea7f958c5c6453fb38ff2
Stacking <mayue8373@live.com>
2022-03-27 15:32:05 +0800

    init

---
+++ Shared/Presets/ColorGroup.Razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Masa.Blazor.Custom.Shared.Presets
+{
+    public partial class ColorGroup
+    {
+        [Parameter]
+        public List<string> Colors { get; set; } = new();
+
+        [Parameter]
+        public string Value { get; set; } = string.Empty;
+
+        [Parameter]
+        public string? Class { get; set; }
+
+        [Parameter]
+        public string? Style { get; set; }
+
+        [Parameter]
+        public int Size { get; set; } = 24;
+
+        [Parameter]
+        public RenderFragment<string>? ItemAppendContent { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            if (Colors.Any())
+            {
+                Value = Colors.First();
+            }
+            base.OnParametersSet();
+        }
+    }
+}
