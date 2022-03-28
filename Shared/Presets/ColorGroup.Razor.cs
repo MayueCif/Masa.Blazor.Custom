42d553f2c8e9c51659eb38ac28841c90c412dc90
Mayue <mayue@lonsid.cn>
2022-03-28 10:23:57 +0800

    fix:bind-value

--- Shared/Presets/ColorGroup.Razor.cs
+++ Shared/Presets/ColorGroup.Razor.cs
@@ -11,6 +11,9 @@ namespace Masa.Blazor.Custom.Shared.Presets
         public string Value { get; set; } = string.Empty;
 
         [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+
+        [Parameter]
         public string? Class { get; set; }
 
         [Parameter]
